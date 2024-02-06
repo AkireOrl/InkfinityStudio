c2bf897146b5fbf4924814f1fb6e5f28b056fde4
erika <om.erikaorlando@gmail.com>
2024-02-06 12:06:27 +0100

    integrado método moment para modificación del formato fecha

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Profile.css";
-import { getProfile } from "../../Services/ApiCalls"; 
+import { getProfile } from "../../Services/ApiCalls";
 import { InputLogin } from "../../Components/InputLogin/InputLogin";
 import { useSelector } from "react-redux";
 import { userData } from "../userSlice";
 import { jwtDecode } from "jwt-decode";
+import moment from "moment";
 
 export const Profile = () => {
   const userRdxDetail = useSelector(userData)
-   console.log(userRdxDetail, "En profile")
+  console.log(userRdxDetail, "En profile")
   const navigate = useNavigate();
   const [profileData, setProfileData] = useState({});
   console.log(profileData, "Linea de arriba qu eno es la 50")
@@ -17,7 +18,7 @@ export const Profile = () => {
   const token = userRdxDetail.credentials.token
   //const decodedToken = jwtDecode(token)
   console.log(token, "Toekn en Profile");
- 
+
 
 
 
@@ -58,42 +59,42 @@ export const Profile = () => {
 
   return (
     <>
-    <div className="profileDesign">
-    <img src={profileData.profileUser?.photo}></img>
-      <h1 className="">{profileData.profileUser?.username}</h1> 
-      <button onClick={() => buttonHandler()}></button>
-      {isEditing 
-      ? (
-        <InputLogin
-          name="firstName"
-          type="text"
-          handler={inputHandler}
-        ></InputLogin>
-      ) : null}
-     
+      <div className="profileDesign">
+        <img src={profileData.profileUser?.photo}></img>
+        <h1 className="">{profileData.profileUser?.username}</h1>
+        <button onClick={() => buttonHandler()}></button>
+        {isEditing
+          ? (
+            <InputLogin
+              name="firstName"
+              type="text"
+              handler={inputHandler}
+            ></InputLogin>
+          ) : null}
 
-      <h2>{profileData.profileUser?.name}</h2>
-      <h2>{profileData.profileUser?.surname}</h2>
-      <p>{profileData.profileUser?.email}</p> 
-      
-      {profileData.userArtistProfiles && profileData.userArtistProfiles.map((userArtistProfiles) => (
-        <div key={userArtistProfiles.id}>
-          <p>{userArtistProfiles.name}</p>
-        </div>
-      ))}
-      {/* <h2>{profileData.userArtistProfiles?.name}</h2> no va */}
-      {profileData.appointments && profileData.appointments.map((appointments) => (
-        <div key={appointments.id}>    
-          <p>  {appointments?.date?.toLocaleDateString("es-ES", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                })}</p>
-          <p>{appointments?.hour}</p>
-        </div>
-      ))}
 
-    </div>
+        <h2>{profileData.profileUser?.name}</h2>
+        <h2>{profileData.profileUser?.surname}</h2>
+        <p>{profileData.profileUser?.email}</p>
+
+        {profileData.userArtistProfiles && profileData.userArtistProfiles.map((userArtistProfiles) => (
+          <div key={userArtistProfiles.id}>
+            <p>{userArtistProfiles.name}</p>
+          </div>
+        ))}
+        {/* <h2>{profileData.userArtistProfiles?.name}</h2> no va */}
+        {profileData.appointments &&
+          profileData.appointments.map((appointment) => (
+            <div key={appointment.id}>
+              {appointment.date ? (
+                <p>{moment(appointment.date).format("DD-MM-YYYY")}</p>
+              ) : null}
+              <p>{appointment?.hour}</p>
+            </div>
+          ))}
+
+
+    </div >
     </>
   );
 };
